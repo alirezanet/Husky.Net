2f42ba1e6a0fdbf61f2dfaf77fee0239038f38fc
AliReZa Sabouri <7004080+alirezanet@users.noreply.github.com>
2021-12-20 16:11:35 +0330

    fix dev build commands

--- src/Husky/global_install.bat
+++
@@ -1,6 +0,0 @@
-dotnet pack -c Release
-@echo off
-dotnet tool uninstall -g husky
-@echo on
-dotnet tool install --global --add-source .\nupkg\ husky
-husky
---
+++ src/Husky/install_global.bat
@@ -0,0 +1,2 @@
+dotnet pack -c Release
+dotnet tool install --global --no-cache --add-source .\nupkg\ husky
