4e79ff0a503aa2541a306306f7ff57621846a37a
AliReZa Sabouri <7004080+alirezanet@users.noreply.github.com>
2021-12-29 03:03:59 +0330

    disable build warnings

--- src/Husky/Globals.cs
+++ src/Husky/Globals.cs
@@ -5,5 +5,9 @@ namespace Husky;
 /// </summary>
 public class Globals
 {
+#pragma warning disable CS8618
+   // ReSharper disable once UnusedAutoPropertyAccessor.Global
    public IList<string> Args { get; set; }
+#pragma warning restore CS8618
+
 }
