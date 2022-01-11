5241dbf2a6e3d701d04dde28c5036c288e2e3107
AliReZa Sabouri <7004080+alirezanet@users.noreply.github.com>
2022-01-11 15:33:30 +0330

    docs: fix typo husky

--- docs/.vuepress/configs/head.ts
+++ docs/.vuepress/configs/head.ts
@@ -10,7 +10,7 @@ export const head: HeadConfig[] = [
    ['meta', { property: 'og:image:type', content: 'image/png' }],
    ['meta', { property: 'og:image:width', content: '1280' }],
    ['meta', { property: 'og:image:height', content: '640' }],
-   ['meta', { property: 'og:title', content: 'Huksy.Net' }],
+   ['meta', { property: 'og:title', content: 'Husky.Net' }],
    ['meta', { property: 'og:type', content: 'website' }],
    ['meta', { property: 'og:url', content: 'https://alirezanet.github.io/Husky.Net/' }],
    ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
