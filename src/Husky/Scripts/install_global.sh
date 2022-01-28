1085a97af43a64e7dbc2e8f848e1f0e96e421b4e
AliReZa Sabouri <alirezanet@outlook.com>
2022-01-28 14:46:08 +0330

    ci: update script install version

--- src/Husky/Scripts/install_global.sh
+++ src/Husky/Scripts/install_global.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 dotnet build ../Husky.csproj
 dotnet pack -c Release ../Husky.csproj
-dotnet tool install -g --no-cache --add-source ../nupkg/ --framework net6.0 --version 0.0.3 husky
+dotnet tool install -g --no-cache --add-source ../nupkg/ --framework net6.0 --version 0.4.0-preview-2 husky
