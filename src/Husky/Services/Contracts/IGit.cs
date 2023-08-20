3fa6f177bd316bf312d1f2a9388e2c88ca500469
Tychus <tychus.mounir@gmail.com>
2023-08-20 16:13:54 +0300

    feat(install): git submodule handling

--- src/Husky/Services/Contracts/IGit.cs
+++ src/Husky/Services/Contracts/IGit.cs
@@ -15,6 +15,21 @@ public interface IGit
    Task<string> GetHuskyPathAsync();
 
    /// <summary>
+   /// Checks if a given path is a submodule.
+   /// <see href="https://git-scm.com/docs/git-rev-parse#Documentation/git-rev-parse.txt---show-superproject-working-tree">relevant git documentation</see>
+   /// </summary>
+   /// <param name="path">path to check</param>
+   /// <returns>true if submodule, false otherwise.</returns>
+   Task<bool> IsSubmodule(string path);
+
+   /// <summary>
+   /// Returns the path to the `.git` directory
+   /// </summary>
+   /// <param name="path">Target path for the git command</param>
+   /// <returns>Path to .git directory if found.</returns>
+   Task<string> GetGitDirectory(string path);
+
+   /// <summary>
    /// only file additions and modifications
    /// </summary>
    /// <returns></returns>
