8cf0e5cf5c234c8acf0bb8960f341e6573ac0b1d
AliReZa Sabouri <alirezanet@outlook.com>
2024-06-16 19:13:36 +0200

    test: add integration tests

--- tests/HuskyIntegrationTests/Issue106Tests.cs
+++ tests/HuskyIntegrationTests/Issue106Tests.cs
@@ -6,10 +6,59 @@ namespace HuskyIntegrationTests;
 public class Issue106Tests (ITestOutputHelper output)
 {
    [Fact]
-   public async Task EchoWithIncludeTask_WhenNoMatchFilesFound_ShouldSkip()
+   public async Task FilteringRuleNotDefined_WithInclude_ShouldNotSkip()
    {
       // arrange
-      await using var c = await ArrangeContainer();
+      const string taskRunner =
+         """
+         {
+             "tasks": [
+                 {
+                     "name": "Echo",
+                     "command": "echo",
+                     "args": [
+                         "Husky.Net is awesome!"
+                     ],
+                     "include": [
+                         "client/**/*"
+                     ]
+                 }
+             ]
+         }
+         """;
+      await using var c = await ArrangeContainer(taskRunner);
+      await c.BashAsync("git add .");
+
+      // act
+      var result = await c.BashAsync(output, "git commit -m 'add task-runner.json'");
+
+      // assert
+      result.ExitCode.Should().Be(0);
+      result.Stderr.Should().NotContain(DockerHelper.Skipped);
+   }
+
+   [Fact]
+   public async Task FilteringRuleNotDefined_WithStagedVariable_WithExcludeCommitedFile_ShouldSkip()
+   {
+      // arrange
+      const string taskRunner =
+         """
+         {
+             "tasks": [
+                 {
+                     "name": "Echo",
+                     "command": "echo",
+                     "args": [
+                         "${staged}"
+                     ],
+                     "exclude": [
+                         "**/task-runner.json"
+                     ]
+                 }
+             ]
+         }
+         """;
+      await using var c = await ArrangeContainer(taskRunner);
       await c.BashAsync("git add .");
 
       // act
@@ -20,32 +69,89 @@ public class Issue106Tests (ITestOutputHelper output)
       result.Stderr.Should().Contain(DockerHelper.Skipped);
    }
 
-   private async Task<IContainer> ArrangeContainer([CallerMemberName] string name = null!)
+   [Fact]
+   public async Task FilteringRuleVariable_WithStagedVariable_WithExcludeCommitedFile_ShouldSkip()
    {
-      var c = await DockerHelper.StartWithInstalledHusky(name);
-      await c.BashAsync("dotnet tool restore");
-      await c.BashAsync("git add .");
-
-      const string tasks =
+      // arrange
+      const string taskRunner =
          """
          {
              "tasks": [
                  {
-                     "name": "EchoWithInclude",
-                     "group": "pre-commit",
-                     "command": "bash",
+                     "name": "Echo",
+                     "command": "echo",
+                     "filteringRule": "variable",
+                     "args": [
+                         "${staged}"
+                     ],
+                     "exclude": [
+                         "**/task-runner.json"
+                     ]
+                 }
+             ]
+         }
+         """;
+      await using var c = await ArrangeContainer(taskRunner);
+      await c.BashAsync("git add .");
+
+      // act
+      var result = await c.BashAsync(output, "git commit -m 'add task-runner.json'");
+
+      // assert
+      result.ExitCode.Should().Be(0);
+      result.Stderr.Should().Contain(DockerHelper.Skipped);
+   }
+
+   [Fact]
+   public async Task FilteringRuleVariable_WithStagedVariable_WithIncludeCommitedFile_ShouldNotSkip()
+   {
+      // arrange
+      const string taskRunner =
+         """
+         {
+             "tasks": [
+                 {
+                     "name": "Echo",
+                     "command": "echo",
+                     "pathMode": "absolute",
+                     "filteringRule": "variable",
+                     "args": [
+                         "${staged}"
+                     ],
+                     "include": [
+                         "**/task-runner.json"
+                     ]
+                 }
+             ]
+         }
+         """;
+      await using var c = await ArrangeContainer(taskRunner);
+      await c.BashAsync("git add .");
+
+      // act
+      var result = await c.BashAsync(output, "git commit -m 'add task-runner.json'");
+
+      // assert
+      result.ExitCode.Should().Be(0);
+      result.Stderr.Should().NotContain(DockerHelper.Skipped);
+      result.Stderr.Should().Contain(".husky/task-runner.json");
+   }
+
+   [Fact]
+   public async Task FilteringRuleStaged_WithoutAnyMatchedFile_ShouldSkip()
+   {
+      // arrange
+      const string taskRunner =
+         """
+         {
+             "tasks": [
+                 {
+                     "name": "Echo",
+                     "command": "echo",
                      "filteringRule": "staged",
                      "args": [
-                         "-c",
-                         "echo Husky.Net is awesome!"
+                         "Husky.Net is awesome!"
                      ],
-                     "windows": {
-                         "command": "cmd",
-                         "args": [
-                             "/c",
-                             "echo Husky.Net is awesome!"
-                         ]
-                     },
                      "include": [
                          "client/**/*"
                      ]
@@ -53,8 +159,121 @@ public class Issue106Tests (ITestOutputHelper output)
              ]
          }
          """;
-      await c.UpdateTaskRunner(tasks);
-      await c.BashAsync("dotnet husky add pre-commit -c 'dotnet husky run -g pre-commit'");
+      await using var c = await ArrangeContainer(taskRunner);
+      await c.BashAsync("git add .");
+
+      // act
+      var result = await c.BashAsync(output, "git commit -m 'add task-runner.json'");
+
+      // assert
+      result.ExitCode.Should().Be(0);
+      result.Stderr.Should().Contain(DockerHelper.Skipped);
+   }
+
+   [Fact]
+   public async Task FilteringRuleStaged_WithoutIncludeAndExclude_ShouldNotSkip()
+   {
+      // arrange
+      const string taskRunner =
+         """
+         {
+             "tasks": [
+                 {
+                     "name": "Echo",
+                     "command": "echo",
+                     "filteringRule": "staged",
+                     "args": [
+                         "Husky.Net is awesome!"
+                     ]
+                 }
+             ]
+         }
+         """;
+      await using var c = await ArrangeContainer(taskRunner);
+      await c.BashAsync("git add .");
+
+      // act
+      var result = await c.BashAsync(output, "git commit -m 'add task-runner.json'");
+
+      // assert
+      result.ExitCode.Should().Be(0);
+      result.Stderr.Should().NotContain(DockerHelper.Skipped);
+   }
+
+   [Fact]
+   public async Task FilteringRuleStaged_WithExcludeCommitedFile_ShouldSkip()
+   {
+      // arrange
+      const string taskRunner =
+         """
+         {
+             "tasks": [
+                 {
+                     "name": "Echo",
+                     "command": "echo",
+                     "filteringRule": "staged",
+                     "args": [
+                         "Husky.Net is awesome!"
+                     ],
+                     "exclude": [
+                         "**/task-runner.json"
+                     ]
+                 }
+             ]
+         }
+         """;
+      await using var c = await ArrangeContainer(taskRunner);
+      await c.BashAsync("git add .");
+
+      // act
+      var result = await c.BashAsync(output, "git commit -m 'add task-runner.json'");
+
+      // assert
+      result.ExitCode.Should().Be(0);
+      result.Stderr.Should().Contain(DockerHelper.Skipped);
+   }
+
+   [Fact]
+   public async Task FilteringRuleStaged_WithIncludeCommitedFile_ShouldNotSkip()
+   {
+      // arrange
+      const string taskRunner =
+         """
+         {
+             "tasks": [
+                 {
+                     "name": "Echo",
+                     "command": "echo",
+                     "filteringRule": "staged",
+                     "args": [
+                         "Husky.Net is awesome!"
+                     ],
+                     "include": [
+                         "**/task-runner.json"
+                     ]
+                 }
+             ]
+         }
+         """;
+      await using var c = await ArrangeContainer(taskRunner);
+      await c.BashAsync("git add .");
+
+      // act
+      var result = await c.BashAsync(output, "git commit -m 'add task-runner.json'");
+
+      // assert
+      result.ExitCode.Should().Be(0);
+      result.Stderr.Should().NotContain(DockerHelper.Skipped);
+   }
+
+
+   private async Task<IContainer> ArrangeContainer(string taskRunner, [CallerMemberName] string name = null!)
+   {
+      var c = await DockerHelper.StartWithInstalledHusky(name);
+      await c.BashAsync("dotnet tool restore");
+      await c.BashAsync("git add .");
+      await c.UpdateTaskRunner(taskRunner);
+      await c.BashAsync("dotnet husky add pre-commit -c 'dotnet husky run'");
       return c;
    }
 }
