8d35aa9afb8ff3f68b4dc609ceaee059f4e9faf5
Weihan Li <weihanli@outlook.com>
2025-11-20 02:00:01 +0800

    remove net6.0/net7.0 since out of support (#135)

--- src/Husky/Scripts/install_global.sh
+++ src/Husky/Scripts/install_global.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 dotnet build ../Husky.csproj
 dotnet pack -c Release ../Husky.csproj
-dotnet tool install -g --no-cache --add-source ../nupkg/ --framework net6.0 --version 0.4.1 husky
+dotnet tool install -g --no-cache --add-source ../nupkg/ --framework net8.0 --version 0.7.2 husky
